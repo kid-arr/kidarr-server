f9b069a10ac9c545d8fed9f0fa81d44163a232ad
Fergal Moran <fergal.moran@gmail.com>
2023-10-10 23:03:53 +0100

    Add map link to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start space-y-6 p-24">
       <h1>I am home</h1>
-            
+      <a href="/map">Map me</a>
     </main>
   );
 }
