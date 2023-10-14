4e1ba1c0baed423de4b7634baab80bf9cfe5e5e0
Fergal Moran <fergal.moran@gmail.com>
2023-10-14 22:05:32 +0100

    shadded

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import MainMap from "@/components/maps/main-map";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("@/components/maps/main-map"), {
+  loading: () => <p>loading...</p>,
+  ssr: false
+})
 
 const MapPage = async () => {
   return (
     <div>
       <h1>Map Page</h1>
       <div>
-        <MainMap />
+        <Map />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start space-y-6 p-24">
       <h1>I am home</h1>
-      <a href="/map">Map me</a>
+      <Button asChild variant={"default"}>
+        <Link href="/map">Map me</Link>
+      </Button>
     </main>
   );
 }
