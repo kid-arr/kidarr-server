626b699dccd63da75e1cfd8354709b21917f0899
Fergal Moran <fergal.moran@gmail.com>
2023-11-22 20:11:36 +0000

    Fix leaflet window not defined error

--- src/app/(parent)/dashboard/page.tsx
+++ src/app/(parent)/dashboard/page.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import MainMap from '@/components/maps/main-map'
+import React, { useMemo } from 'react'
 import ChildrenFilter from '@/components/children/children-filter'
+import dynamic from 'next/dynamic'
 
 const DashboardPage = async () => {
+
+  const Map = dynamic(() => import('../../../components/maps/main-map'), { ssr: false })
   return (
     <div>
-      <div className="z-10">
+      <div className='z-10'>
         <ChildrenFilter />
       </div>
       <div>This is the dashboard</div>
-      <div className="z-0">
-        <MainMap />
+      <div className='z-0'>
+        <Map />
       </div>
     </div>
   )
--- src/components/maps/main-map.tsx
+++ src/components/maps/main-map.tsx
@@ -1,23 +1,32 @@
 'use client'
 import 'leaflet/dist/leaflet.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
+import { Socket, io } from 'socket.io-client'
 
 const MainMap = () => {
+
+  const [isMounted, setIsMounted] = React.useState(false)
+  React.useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   return (
-    <div>
-      <MapContainer
-        className="map"
-        center={[51.903614, -8.468399]}
-        zoom={20}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        ></TileLayer>
-      </MapContainer>
-    </div>
+    isMounted && (
+      <div>
+        <MapContainer
+          className='map'
+          center={[51.903614, -8.468399]}
+          zoom={20}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          ></TileLayer>
+        </MapContainer>
+      </div>
+    )
   )
 }
 
