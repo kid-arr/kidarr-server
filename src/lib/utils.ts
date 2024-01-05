b58c3276a40191fd285f3a3a25d6b857091a1701
Fergal Moran <fergal.moran@gmail.com>
2024-01-05 21:00:28 +0000

    Add default avatar and only show latest ping

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,12 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import PingModel from '@/lib/models/ping';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getInitials = (name: string) => {
-  return name?.match(/(\b\S)?/g).join('');
-};
+export const getInitials = (name: string) => name?.match(/(\b\S)?/g)?.join('') ?? '';
+
+export const getLatestPing = (pings: PingModel[]): PingModel =>
+  pings.reduce((ping, current) => (ping.timestamp > current.timestamp ? ping : current));
