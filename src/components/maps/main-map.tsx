5c2aad1846f8059110be2f0e3558e3c4d56088eb
Fergal Moran <fergal.moran@gmail.com>
2024-01-19 11:43:57 +0000

    Route drawing working (still need to toggle)

--- src/components/maps/main-map.tsx
+++ src/components/maps/main-map.tsx
@@ -1,7 +1,14 @@
 "use client";
 import "leaflet/dist/leaflet.css";
 import React, { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer, Circle } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Circle,
+  Polyline,
+} from "react-leaflet";
 import { usePingSocket } from "@/lib/hooks/use-ping-socket";
 import type ChildModel from "@/lib/models/child";
 import MapMarker from "@/components/maps/map-marker";
@@ -16,7 +23,40 @@ const MainMap: React.FC<MainMapProps> = ({ kids }) => {
     setIsMounted(true);
     console.log("MainMap", "kids", kids);
   }, [kids]);
-
+  // Draw lines between each ping showing the direction of travel
+  const renderLines = (kids: ChildModel[]) => {
+    return kids?.map((kid) =>
+      kid.devices?.map((device) =>
+        device.pings?.map((ping, index) => {
+          const nextPing = device.pings[index + 1];
+          if (nextPing) {
+            return (
+              <>
+                <Polyline
+                  key={`${ping.id}-${nextPing.id}`}
+                  positions={[
+                    [ping.latitude, ping.longitude],
+                    [nextPing.latitude, nextPing.longitude],
+                  ]}
+                />
+                <MapMarker
+                  key={ping.id}
+                  childName={kid.name}
+                  avatar={kid.avatar}
+                  deviceId={device.id}
+                  deviceName={device.deviceName}
+                  latitude={ping.latitude}
+                  longitude={ping.longitude}
+                  timestamp={ping.timestamp}
+                />
+              </>
+            );
+          }
+          return null;
+        }),
+      ),
+    );
+  };
   return (
     isMounted && (
       <div>
@@ -28,25 +68,56 @@ const MainMap: React.FC<MainMapProps> = ({ kids }) => {
         >
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <>
-            {kids?.map(
-              (kid) =>
-                kid.devices?.map((device) => {
-                  const latestPing = getLatestPing(device.pings);
-                  return (
-                    latestPing && (
-                      <MapMarker
-                        key={latestPing.id}
-                        childName={kid.name}
-                        avatar={kid.avatar}
-                        deviceId={device.id}
-                        deviceName={device.deviceName}
-                        latitude={latestPing.latitude}
-                        longitude={latestPing.longitude}
-                        timestamp={latestPing.timestamp}
-                      />
-                    )
+            {renderLines(kids)}
+            {kids?.map((kid) =>
+              kid.devices?.map((device) => {
+                const latestPing = getLatestPing(device.pings);
+
+                return (
+                  latestPing && (
+                    <MapMarker
+                      key={latestPing.id}
+                      childName={kid.name}
+                      avatar={kid.avatar}
+                      deviceId={device.id}
+                      deviceName={device.deviceName}
+                      latitude={latestPing.latitude}
+                      longitude={latestPing.longitude}
+                      timestamp={latestPing.timestamp}
+                    />
+                  )
+                );
+
+                // Draw coordinates of each ping on the map
+                const renderPings = () => {
+                  return kids?.map((kid) =>
+                    kid.devices?.map((device) =>
+                      device.pings?.map((ping) => (
+                        <Circle
+                          key={ping.id}
+                          center={[ping.latitude, ping.longitude]}
+                          radius={100} // Adjust the radius as needed
+                        />
+                      )),
+                    ),
                   );
-                }),
+                };
+
+                return (
+                  latestPing && (
+                    <MapMarker
+                      key={latestPing.id}
+                      childName={kid.name}
+                      avatar={kid.avatar}
+                      deviceId={device.id}
+                      deviceName={device.deviceName}
+                      latitude={latestPing.latitude}
+                      longitude={latestPing.longitude}
+                      timestamp={latestPing.timestamp}
+                    />
+                  )
+                );
+              }),
             )}
           </>
         </MapContainer>
