f03bf480327baddecc492212bbd31ba2eaf15502
Fergal Moran <fergal.moran@gmail.com>
2024-01-30 15:39:42 +0000

    Cleanup ping route

--- src/lib/api/devices/queries.ts
+++ src/lib/api/devices/queries.ts
@@ -1,19 +1,30 @@
 import { db } from "@/server/db/index";
 import { eq, and } from "drizzle-orm";
 import { getUserAuth } from "@/lib/auth/utils";
-import { type DeviceId, deviceIdSchema, devices } from "@/server/db/schema/devices";
+import {
+  type DeviceId,
+  deviceIdSchema,
+  devices,
+} from "@/server/db/schema/devices";
 import { children } from "@/server/db/schema/children";
 
 export const getDevices = async () => {
   const { session } = await getUserAuth();
-  const d = await db.select({ device: devices, child: children }).from(devices).leftJoin(children, eq(devices.childId, children.id)).where(eq(devices.userId, session?.user.id!));
+  const d = await db
+    .select({ device: devices, child: children })
+    .from(devices)
+    .leftJoin(children, eq(devices.childId, children.id))
+    .where(eq(devices.userId, session?.user.id!));
   return { devices: d };
 };
 
 export const getDeviceById = async (id: DeviceId) => {
   const { session } = await getUserAuth();
   const { id: deviceId } = deviceIdSchema.parse({ id });
-  const [d] = await db.select().from(devices).where(and(eq(devices.id, deviceId), eq(devices.userId, session?.user.id!))).leftJoin(children, eq(devices.childId, children.id));
+  const [d] = await db
+    .select()
+    .from(devices)
+    .where(and(eq(devices.id, deviceId), eq(devices.userId, session?.user.id!)))
+    // .leftJoin(children, eq(devices.childId, children.id));
   return { device: d };
 };
-
---
+++ src/lib/api/responses.ts
@@ -0,0 +1,10 @@
+import { StatusCodes } from "http-status-codes";
+
+export const notAuthorised = () =>
+  new Response("Not authorised", {
+    status: StatusCodes.BAD_REQUEST,
+  });
+export const badRequest = (message: string) =>
+  new Response(message, {
+    status: StatusCodes.BAD_REQUEST,
+  });
