de4a6ff0a747ebe642cb3cdfe16f9623b7bf01fd
Fergal Moran <fergal.moran@gmail.com>
2024-01-31 20:00:15 +0000

    Fix debuggles

--- src/app/debug/page.tsx
+++ src/app/debug/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
+import HeadersPrinter from "@/components/debug/headers-printer";
+import { SecureDebugDetails } from "@/components/debug/secure-debug-details";
 
 const DebugPage = () => {
   return (
     <div className="p-8">
       <Button variant="default">I am button</Button>
+      <div className="flex flex-col space-y-2 px-2">
+        <HeadersPrinter />
+        <SecureDebugDetails />
+      </div>
     </div>
   );
 };
